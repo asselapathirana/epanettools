85f9dfe71571b973fbd3c5b1f51dc919639fc68b
Assela Pathirana <assela@pathirana.net>
2015-10-09 20:53:26 +0200

    Link and Node lists are returned. Tests pass

--- epanettools/epanettools.py
+++ epanettools/epanettools.py
@@ -1,4 +1,4 @@
-from epanettools import epanet2 as et
+from . import epanet2 as et
 import tempfile, shutil, os
 
 
@@ -6,14 +6,58 @@ class EPANetSimulation():
     
     
     def __init__(self,inputFileName):
-        if(et.ENopen(inputFileName,"tmp.rpt","tmp.dat")):
-            raise FileNotFoundError("File %s not found" % inputFileName)
+        self.enOpenStatus=False
         self.OriginalInputFileName=inputFileName
         self.inputfile=self.create_temporary_copy(inputFileName)
+        self.rptfile=self.inputfile[:-3]+".rpt"
+        self.binfile=self.inputfile[:-3]+".bin"
+        self._open()
+
+    
+    def Error(self,e):
+        if(e):
+            s="Epanet Error: %d : %s" %(ENgeterror(e,50),e)
+            raise Exception(s)        
+            
+    def create_temporary_copy(self,path):    
+        f=os.path.join(tempfile._get_default_tempdir(),next(tempfile._get_candidate_names())+".inp")
+        shutil.copyfile(path,f)
+        return f
+    def _open(self): 
+        if(not self.enOpenStatus):
+            self._explicitly_open()
+        self.enOpenStatus=True
+
+    def _explicitly_open(self):
+        self.Error(et.ENopen(self.inputfile,self.rptfile,self.binfile))
+
         
-    def create_temporary_copy(path):
-        temp_dir = tempfile.gettempdir()
-        temp_path = os.path.join(temp_dir, 'temp_file_name')
-        shutil.copy2(path, temp_path)
-        return temp_path
+    def _reset(self):
+        self.Error(et.ENclose())
+        self.Error(et._explicitly_open())
+        
+        
+    def getLinks(self):
+        self._open()
+        self.links=[]
+        for i in range(1,et.ENgetcount(et.EN_LINKCOUNT)[1]+1):
+            self.links.append(et.ENgetlinkid(i)[1])
+        return self.links
+    
+    def getNodes(self):
+        self._open()
+        self.nodes=[]
+        for i in range(1,et.ENgetcount(et.EN_NODECOUNT)[1]+1):
+            self.nodes.append(et.ENgetnodeid(i)[1])
+        return self.nodes 
+    
+    def __getattribute__(self, name):
+        try:
+            return object.__getattribute__(self, name)
+        except:
+            pass
+        self._open()
+        if(hasattr(et,name)): # search legacy interface            
+            return getattr(et,name)
+        raise AttributeError("The attribute %s not found with this class or underlying c interface" % name)    
         
--- tests/test_EPANetSimulation.py
+++ tests/test_EPANetSimulation.py
@@ -1,15 +1,19 @@
 import os
 import unittest
 import epanettools
+from epanettools.examples import simple
+from  epanettools.epanettools import EPANetSimulation
 
 from unittest import skip, expectedFailure
 
 
 class Test1(unittest.TestCase):
-    def setup(): 
+    def setUp(self): 
         print("SETUP!")
+        file = os.path.join(os.path.dirname(simple.__file__),'Net3.inp')
+        self.es=EPANetSimulation(file)           
     
-    def teardown():
+    def subTest(self):
         print("TEAR DOWN!")
     
     @skip  
@@ -21,24 +25,48 @@ class Test1(unittest.TestCase):
             from epanettools.epanettools import EPANetSimulation
         except (Exception):
             assert False
+            
+    def test_non_existing_file_raise_error(self):
+        self.assertRaises(FileNotFoundError, EPANetSimulation,"Silly file")
     
     def test_properly_open_a_network_file(self):
+        import filecmp
+        file = os.path.join(os.path.dirname(simple.__file__),'Net3.inp')
+        es=EPANetSimulation(file)
+        self.assertNotEqual(file,self.es.inputfile)
+        self.assertTrue(os.path.isfile(self.es.inputfile))
+        self.assertFalse(os.path.isdir(self.es.inputfile))
+        # file names are unique
+        self.assertEqual(len(set([EPANetSimulation(file).inputfile for i in range(100)])),100)
+        # file content is identical to the original file
+        self.assertTrue(filecmp.cmp(self.es.inputfile,file))
+        # but names are not the same
+        self.assertFalse(self.es.inputfile==file)
+        
+    def test_get_correct_network_information(self):
         from  epanettools.epanettools import EPANetSimulation
         from epanettools.examples import simple
         
-        self.assertRaises(FileNotFoundError, EPANetSimulation,"Silly file")
-        file = os.path.join(os.path.dirname(simple.__file__),'Net3.inp')
-        es=EPANetSimulation(file)
-        self.assertNotEqual(file,es.inputfile)
-        self.assertTrue(os.path.isfile(es.inputfile))
-        self.assertFalse(os.path.isdir(es.inputfile))
+
+        self.assertEqual(self.es.getNodes()[0],'10')
+        self.assertEqual(self.es.getNodes()[2],'20')
+        self.assertEqual(self.es.getNodes()[-1],'3')
+        self.assertEqual(self.es.getNodes()[-4],'Lake')
+
+        self.assertEqual(self.es.getLinks()[0],'20')
+        self.assertEqual(self.es.getLinks()[2],'50')
+        self.assertEqual(self.es.getLinks()[-1],'335')    
         
+    def can_access_low_level_EN_type_functions(self):
+        self.assertEqual(self.es.ENgetnodeid(3),[0,'20'])
     
 def main():
     #test_false()
     tc=Test1()
-    
-    tc.test_properly_open_a_network_file()
+    tc.setUp()
+    tc.test_non_existing_file_raise_error()
+    tc.can_access_low_level_EN_type_functions()
+    tc.test_get_correct_network_information()
 
 if __name__ == "__main__":
         main()
