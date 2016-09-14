019b6aa3d05f760b888625bb9021df9aef2cdc92
ak2ls2py <python2.learnsys@gmail.com>
2016-09-14 14:09:03 -0400

    Now PDD is working. tests pass

--- epanettools/pdd_class_wrapper.py
+++ epanettools/pdd_class_wrapper.py
@@ -2,11 +2,9 @@ from . import pdd as pd
 class pdd_wrapper_class(object):
     
     def __init__(self,pdd=False):
+        self.pdd=pdd #THIS MUST BE the first statement. 
         self.pd=pd
-        self.set_pdd(pdd)
     
-    def set_pdd(self,pdd=False):
-        self.pdd=pdd
         
     def __getattribute__(self, name):
         try:
@@ -17,13 +15,13 @@ class pdd_wrapper_class(object):
         if(name[-5:]=="_wrap"): # then the name already has wrap
             f=name
         else:
-            if(self.pdd):
+            if(self.pdd and name[:2]=="EN" and str.upper(name)!=name and name[2]!='_'):
                 f=name+"_wrap" 
             else:
                 f=name
         if(hasattr(self.pd,name)): #
             #print ("Name:"+name+" f="+f)
-            return getattr(self.pd,name)
+            return getattr(self.pd,f)
         raise AttributeError("The attribute %s not found with this class or underlying c interface" % name) 
             
     
