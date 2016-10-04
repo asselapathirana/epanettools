66e35998bec61efd7e98684bb8fa51c5f2330236
ak2ls2py <python2.learnsys@gmail.com>
2016-10-04 08:52:57 -0400

    Revert "trying to get full tox to pass"
    This reverts commit 37a122a8745045274a8fa974cbb3a5fd2c7c615a.

--- setup.py
+++ setup.py
@@ -4,23 +4,25 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import io
-# import sys
+import sys
 import os
 import re
 from glob import glob
 from os.path import basename
 from os.path import dirname
 from os.path import join
-# from os.path import relpath
+from os.path import relpath
 from os.path import splitext
 
 from setuptools import Extension
-# from setuptools import find_packages
+from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
-# from setuptools.command.build_ext import build_ext
+from setuptools.command.build_ext import build_ext
 
-# from collections import defaultdict
+from collections import defaultdict
+
+import numpy
 
 
 class Tox(TestCommand):
@@ -98,7 +100,7 @@ setup(
     author_email='assela@pathirana.net',
     url='https://github.com/asselapathirana/epanettools',
     packages=["epanettools"],
-    include_dirs=[],
+    include_dirs=[numpy.get_include()],
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
@@ -129,6 +131,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     install_requires=[
+         'numpy>=1.0'
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
@@ -155,5 +158,5 @@ setup(
                   include_dirs=["src/epanettools/pdd", "src/epanettools/epanet"]
                   )
     ],
-    tests_require=['tox'],
+        tests_require=['tox'],
 )
